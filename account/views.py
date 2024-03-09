60c0e0767a40cb34de6d75f7045013028d103ea7
Lewis Tough <2770287t@student.gla.ac.uk>
2024-03-09 15:28:09 +0000

    view for profiles

--- account/views.py
+++ account/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from .models import CustomUser
+from game.models import GameReview
 
 # Create your views here.
 
-
+@login_required
 def profilePage(request):
-    # viewing own profile
-    return render(request, 'account/profile.html')
+    #viewing own profile
+    user_info = request.user
+    user_reviews = GameReview.objects.filter(created_by=request.user)
+
+    context_dict = {
+        'user_info': user_info,
+        'user_reviews': user_reviews,
+    }
+
+    return render(request, 'account/profile.html', context_dict)
 
 
-def viewProfile(request, id):
-    # viewing other user's profile
+def viewProfile(request, username):
+    user_info = get_object_or_404(CustomUser, username=username)
+    user_reviews = GameReview.objects.filter(created_by=user_info)
 
-    context_dict = {'id': id}
+    context_dict = {'user_info': user_info,
+                    'user_reviews': user_reviews}
 
     return render(request, 'account/viewProfile.html', context_dict)
 
