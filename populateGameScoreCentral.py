f704018c9af9b13c0f5f98e5842d476bef652309
Joe Paechter <2667501p@student.gla.ac.uk>
2024-03-13 13:12:10 +0000

    new file:   media/game_posters/Baldurs.jpg 	new file:   media/game_posters/MW3.jpg 	new file:   media/game_posters/SuperMarioWonder.jpg 	modified:   populateGameScoreCentral.py

--- populateGameScoreCentral.py
+++ populateGameScoreCentral.py
@@ -1,19 +1,21 @@
+from datetime import datetime
 import os
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'GameScoreCentral.settings'
+django.setup()
 from django.conf import settings
 from django.core.files import File
 from django.contrib.auth.models import User
 from django.conf import settings
-os.environ('DJANGO_SETTINGS_MODULE',
-           'GameScoreCentral.settings')
-import django
-django.setup()
+from django.utils import timezone
 from game.models import Game, GameReview
 from category.models import GameCategory
+from account.models import CustomUser
 
 def populate():
     media_url = settings.MEDIA_URL
-    user1 = User.objects.create_user(username='user1',password='password1')
-    user2 = User.objects.create_user(username='user2',password='password2')
+    user1 = CustomUser.objects.create_user(username='user1',password='password1')
+    user2 = CustomUser.objects.create_user(username='user2',password='password2')
 
     rpg_games = [
         {'title': 'The Elder Scrols V: Skyrim',
@@ -23,19 +25,67 @@ def populate():
          'game_studio': 'Bethesda',
          'posted_by':user1,
          'average_review': 9,
+        },
+        {'title': 'Balders Gate 3',
+         'description': "The game is the third main installment in the Baldur's Gate series, based on the tabletop fantasy role-playing system of Dungeons & Dragons.",
+         'release': 2023,
+         'poster': os.path.join(media_url, 'game_posters', 'Baldurs.jpg'),
+         'game_studio': 'Larian Studios',
+         'posted_by':user2,
+         'average_review': 0,
         }
     ]
+
+    fps_games = [
+        {'title': 'Call of Duty: Modern Warfare 3',
+         'description': 'Call of Duty: Modern Warfare 3 is a 2011 first-person shooter video game',
+         'release': 2011,
+         'poster': os.path.join(media_url, 'game_posters', 'MW3.jpg'),
+         'game_studio': 'Infinity Ward',
+         'posted_by':user2,
+         'average_review': 8,
+
+        }
+    ]
+    
+    platforming_games = [
+        {
+         'title': 'Super Mario Bros. Wonder',
+         'description': 'The player controls Mario, Luigi, and their friends as they attempt to stop Bowser, who plots to take over a new land known as the Flower Kingdom after using the magical Wonder Flower to fuse himself with the kingdoms castle.',
+         'release': 2023,
+         'poster': os.path.join(media_url, 'game_posters', 'SuperMarioWonder.jpg'),
+         'game_studio': 'Nintendo',
+         'posted_by':user1,
+         'average_review': 10,
+        } 
+        
+    ]
+
+
 
     review_details = [
         {'rating': 9,
          'comment': "Its good",
-         'created by':user2,
+         'created_by':user2,
          'game': 'The Elder Scrols V: Skyrim',
          'created_at':'2024-03-11 12:00:00'
+        },
+        {'rating': 8,
+         'comment': "Its pretty good, great shooting",
+         'created_by':user1,
+         'game': 'Call of Duty: Modern Warfare 3',
+         'created_at':'2024-03-11 12:00:00'
+        },
+        {'rating': 10,
+         'comment': "It changed my life",
+         'created_by':user1,
+         'game': 'Super Mario Bros. Wonder',
+         'created_at':'2024-03-11 12:00:00'
         }
+
     ]
 
-    categories = {'RPG':{'games':rpg_games},}
+    categories = {'RPG':{'games':rpg_games}, 'FPS':{'games': fps_games}, 'Platformers':{'games': platforming_games}}
     for cat, cat_data in categories.items():
         c = add_cat(cat)
         for g in cat_data['games']:
@@ -43,7 +93,7 @@ def populate():
         
     for r in review_details:
         game_title = r['game']
-        game = Game.Objects.get(title=game_title)
+        game = Game.objects.get(title=game_title)
         add_review(r['rating'],r['comment'],r['created_by'],game,r['created_at'])
 
         
@@ -51,27 +101,25 @@ def populate():
     
 
 def add_game(title, description, release, poster,category,game_studio,posted_by,average_review):
-    g = Game.objects.get_or_create(title=title, category=category)[0]
-    g.description = description
-    g.release = release
-    g.poster = poster
-    g.game_studio = game_studio
-    g.posted_by = posted_by
-    g.average_review = average_review
+    g = Game.objects.get_or_create(title=title, category=category, release_data=release,description = description,poster = poster, game_studio = game_studio, posted_by = posted_by, average_review = average_review)[0]
     g.save()
     return g
 
 def add_review(rating,comment,created_by,game,created_at):
-    r = GameReview.objects.get_or_create(game=game,created_by=created_by)[0]
-    r.rating = rating
-    r.comment = comment
-    r.created_at = created_at
+    created_at_datetime = datetime.strptime(created_at, '%Y-%m-%d %H:%M:%S')
+    created_at_aware = timezone.make_aware(created_at_datetime)
+    r = GameReview.objects.get_or_create(game=game,created_by=created_by, rating = rating, comment=comment, created_at=created_at_aware)[0]
+    
     r.save()
 
 def add_cat(name):
     c = GameCategory.objects.get_or_create(title=name)[0]
     c.save()
     return c
+
+if __name__ == '__main__':
+    print('Starting population script...')
+    populate()
 
 
     
