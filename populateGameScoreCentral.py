29ee50b303fca63613157a84b5a6c7fed686575d
Joe Paechter <2667501p@student.gla.ac.uk>
2024-03-19 21:58:54 +0000

    new file:   media/game_posters/Apex.jpg 	new file:   media/game_posters/Celeste.jpg 	new file:   media/game_posters/Doom.jpg 	new file:   media/game_posters/Invaders.jpg 	new file:   media/game_posters/ItsSoOver.jpg 	new file:   media/game_posters/Pearl.jpg 	new file:   media/game_posters/Smash.jpg 	new file:   media/game_posters/Sonic.jpg 	new file:   media/game_posters/Street.jpg 	new file:   media/game_posters/Tekken.jpg 	new file:   media/game_posters/Tetris.jpg 	modified:   populateGameScoreCentral.py

--- populateGameScoreCentral.py
+++ populateGameScoreCentral.py
@@ -33,7 +33,16 @@ def populate():
          'game_studio': 'Larian Studios',
          'posted_by':user2,
          'average_review': 0,
-        }
+        },
+        {'title': 'Pokemon Pearl',
+         'description': "Pokémon Pearl Version is a role-playing video game developed by Game Freak and published by The Pokémon Company and Nintendo for the Nintendo DS in 2006. It is one of the first installments in the fourth generation of the Pokémon video game series.",
+         'release': 2006,
+         'poster': os.path.join(media_url, 'game_posters', 'Pearl.jpg'),
+         'game_studio': 'Game Freak',
+         'posted_by':user1,
+         'average_review': 0,
+        },
+        
     ]
 
     fps_games = [
@@ -45,7 +54,34 @@ def populate():
          'posted_by':user2,
          'average_review': 8,
 
-        }
+        },
+         {'title': 'Apex Legends',
+         'description': 'Apex legends is a free-to-play, fastbased battle royale. Pick your favourite hero and fight for glory!',
+         'release': 2019,
+         'poster': os.path.join(media_url, 'game_posters', 'Apex.jpg'),
+         'game_studio': 'Respawn Entertainment',
+         'posted_by':user1,
+         'average_review': 0,
+
+        },
+        {'title': 'Doom Eternal',
+         'description': 'Doom Eternal is a first-person shooter straight from hell. slach, shoot and punch through demons in this engergetic sequal to the critically acclaimed Doom (2016)',
+         'release': 2020,
+         'poster': os.path.join(media_url, 'game_posters', 'Doom.jpg'),
+         'game_studio': 'id Software',
+         'posted_by':user1,
+         'average_review': 0,
+
+        },
+        {'title': 'Overwatch',
+         'description': 'Overwatch is a competative online hero shooter.',
+         'release': 2016,
+         'poster': os.path.join(media_url, 'game_posters', 'ItsSoOver.jpg'),
+         'game_studio': 'Blizzard Entertainment',
+         'posted_by':user2,
+         'average_review': 0,
+
+        },
     ]
     
     platforming_games = [
@@ -57,10 +93,80 @@ def populate():
          'game_studio': 'Nintendo',
          'posted_by':user1,
          'average_review': 10,
-        } 
+        },
+        {
+         'title': 'Sonic The Hedgehog (2006)',
+         'description': 'Sonic the Hedgehog (2006) was realeased as a celebration of the 15th aniversary of the sonic franchise, in the hope to reboot the series',
+         'release': 2006,
+         'poster': os.path.join(media_url, 'game_posters', 'Sonic.jpg'),
+         'game_studio': 'Sonic Team',
+         'posted_by':user2,
+         'average_review': 0,
+        },
+        {
+         'title': 'Celeste',
+         'description': 'Celeste is a indie platforming game where you help the protagnist Madoline climb celeste mountain and battle her depresssion and anxiety',
+         'release': 2018,
+         'poster': os.path.join(media_url, 'game_posters', 'Celeste.jpg'),
+         'game_studio': 'Maddy Makes Games',
+         'posted_by':user1,
+         'average_review': 0,
+        }
         
     ]
 
+    arcade_games = [
+        {
+         'title': 'Tetris',
+         'description': 'Tetris is a arcade game where you are tasked with arraning falling blocks. Try and last as long as you can in this never ending classic',
+         'release': 1985,
+         'poster': os.path.join(media_url, 'game_posters', 'Tetris.jpg'),
+         'game_studio': 'Alexey Paitnov',
+         'posted_by':user2,
+         'average_review': 10,
+
+        },
+        {
+         'title': 'Space invaders',
+         'description': 'Shoot down aliens and defend your planet in this classic sci-fi shooter game',
+         'release': 1978,
+         'poster': os.path.join(media_url, 'game_posters', 'Invaders.jpg'),
+         'game_studio': 'Taito',
+         'posted_by':user1,
+         'average_review': 0,
+
+        },
+    ]
+
+    fighting_games = [
+        {
+         'title': 'Street Fighter 6',
+         'description': 'Street Fighters combat is as good at ever in this fresh modern take on the series',
+         'release': 2023,
+         'poster': os.path.join(media_url, 'game_posters', 'Street.jpg'),
+         'game_studio': 'Capcom',
+         'posted_by':user2,
+         'average_review': 7,
+        },
+        {
+         'title': 'Tekken 7',
+         'description': 'Fight your way through Tekken 7s coulourful cast of characters in this awesome 3-d fighting game',
+         'release': 2015,
+         'poster': os.path.join(media_url, 'game_posters', 'Tekken.jpg'),
+         'game_studio': 'Bandai Namco Entertainment',
+         'posted_by':user1,
+         'average_review': 0,
+        },
+        {
+         'title': 'Super Smash Bros. Ultimate ',
+         'description': 'Smash is as good as ever with its newest installment on the Nintendo Switch, hop on with up to 7 of your friends for awesome cross-over fighting madness',
+         'release': 2018,
+         'poster': os.path.join(media_url, 'game_posters', 'Smash.jpg'),
+         'game_studio': 'Nintendo',
+         'posted_by':user2,
+         'average_review': 0,
+        },
+    ]
 
 
     review_details = [
@@ -81,11 +187,29 @@ def populate():
          'created_by':user1,
          'game': 'Super Mario Bros. Wonder',
          'created_at':'2024-03-11 12:00:00'
-        }
+        },
+        {'rating': 0,
+         'comment': "It ruined my Life",
+         'created_by':user2,
+         'game': 'Sonic The Hedgehog (2006)',
+         'created_at':'2024-03-11 12:00:00'
+        },
+         {'rating': 7,
+         'comment': "I like this game, but I'm bad at it :(",
+         'created_by':user1,
+         'game': 'Street Fighter 6',
+         'created_at':'2024-03-11 12:00:00'
+        },
+        {'rating': 10,
+         'comment': "I love blocks!",
+         'created_by':user2,
+         'game':'Tetris',
+         'created_at':'2024-03-11 12:00:00'
+        },
 
     ]
 
-    categories = {'RPG':{'games':rpg_games}, 'FPS':{'games': fps_games}, 'Platformers':{'games': platforming_games}}
+    categories = {'RPG':{'games':rpg_games}, 'FPS':{'games': fps_games}, 'Platformers':{'games': platforming_games}, 'Arcade Games':{'games': arcade_games}, 'Fighting Games':{'games': fighting_games}}
     for cat, cat_data in categories.items():
         c = add_cat(cat)
         for g in cat_data['games']:
